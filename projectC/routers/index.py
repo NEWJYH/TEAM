ca0edd12d4a21c8cb92a1e4508295f823cc139b7
dlswns6532 <hij092919@gmail.com>
2022-11-23 10:24:25 +0900

    Rename main.j2,js,css to graph.j2,js,css & update those

--- projectC/routers/index.py
+++ projectC/routers/index.py
@@ -10,7 +10,7 @@ from datetime import date, timedelta
 import random
 
 router = APIRouter(
-    prefix="/index",
+    prefix="/graph",
 )
 
 templates = Jinja2Templates(directory="templates")
@@ -19,15 +19,7 @@ templates = Jinja2Templates(directory="templates")
 async def index(request: Request):
     today = date.today()
     defaultStart = today - timedelta(29)
-    dayLabel = list(range(1, (today - defaultStart).days + 1))
     randomlist = [random.choices(range(100, 200), k=(today - defaultStart).days+1), random.choices(range(100, 200), k=(today - defaultStart).days+1), random.choices(range(100, 200), k=(today - defaultStart).days+1), random.choices(range(100, 200), k=(today - defaultStart).days+1), random.choices(range(100, 200), k=(today - defaultStart).days+1),]
-    return templates.TemplateResponse("main.j2", context={'request':request, 'today':today, 'defaultStart':defaultStart, 'dayLabel':dayLabel, 'randomlist':randomlist})
-
-@router.post('/post', response_class=HTMLResponse, status_code=status.HTTP_202_ACCEPTED)
-async def post(request: Request):
-    context = {}
-    context['request'] = request
-    context['dada'] = 'dadaad'
-    return context
+    return templates.TemplateResponse("graph.j2", context={'request':request, 'today':today, 'defaultStart':defaultStart, 'randomlist':randomlist})
 
 
