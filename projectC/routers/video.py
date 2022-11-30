96d267c4461f15ae4d50817a7cebfa1a804d656b
dlswns6532 <hij092919@gmail.com>
2022-11-30 12:50:26 +0900

    test video

--- projectC/routers/video.py
+++ projectC/routers/video.py
@@ -23,31 +23,43 @@ router = APIRouter(
 templates = Jinja2Templates(directory="templates")
 
 CHUNK_SIZE = 1024*1024
-video_path = Path("./video/detect/cowfarmB_ch1_2022072519_003.mp4")
+# video_path = Path("test_sample.webm")
+mapvideo_path = Path("test.webm")
 
 
 @router.get("/")
 async def read_root(request: Request):
-    return templates.TemplateResponse("test.html", context={"request": request})
+    return templates.TemplateResponse("test.j2", context={"request": request})
 
+# @router.get("/video")
+# async def video_endpoint(range: str = Header(None)):
+#     start, end = range.replace("bytes=", "").split("-")
+#     start = int(start)
+#     end = int(end) if end else start + CHUNK_SIZE
+#     with open(video_path, "rb") as video:
+#         video.seek(start)
+#         data = video.read(end - start)
+#         filesize = str(video_path.stat().st_size)
+#         headers = {
+#             'Content-Range': f'bytes {str(start)}-{str(end)}/{filesize}',
+#             'Accept-Ranges': 'bytes'
+#         }
+#         return Response(data, status_code=206, headers=headers, media_type="video/webm")
 
-@router.get("/video")
+@router.get("/mapvideo")
 async def video_endpoint(range: str = Header(None)):
-    print('응답이 왔어요')
-    print(range)
     start, end = range.replace("bytes=", "").split("-")
-    # print(start, end)
     start = int(start)
     end = int(end) if end else start + CHUNK_SIZE
-    with open(video_path, "rb") as video:
+    with open(mapvideo_path, "rb") as video:
         video.seek(start)
         data = video.read(end - start)
-        filesize = str(video_path.stat().st_size)
+        filesize = str(mapvideo_path.stat().st_size)
         headers = {
             'Content-Range': f'bytes {str(start)}-{str(end)}/{filesize}',
             'Accept-Ranges': 'bytes'
         }
-        return Response(data, status_code=206, headers=headers, media_type="video/mp4")
+        return Response(data, status_code=206, headers=headers, media_type="video/webm")
 
 
 
