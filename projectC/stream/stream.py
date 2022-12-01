0335f30bd2ef4ca981e10c8480539a1e135e8b26
jyh <joung5846@naver.com>
2022-12-01 09:18:12 +0900

    merge complete

--- projectC/stream/stream.py
+++ projectC/stream/stream.py
@@ -1,17 +1,12 @@
 import cv2
-import time 
+
 def get_stream_video():
     # camera 정의
     video_input_path = 'video/detect/ch3_rtsp.mp4'
-
     cam = cv2.VideoCapture(video_input_path)
-    cnt = 0
-    a = time.time()
     while True:
-        cnt += 1
         # 카메라 값 불러오기
         success, frame = cam.read()
-
         if not success:
             break
         else:
@@ -19,7 +14,4 @@ def get_stream_video():
             # frame을 byte로 변경 후 특정 식??으로 변환 후에
             # yield로 하나씩 넘겨준다.
             frame = buffer.tobytes()
-            print(cnt)
             yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(frame) + b'\r\n')
-    b = time.time()
-    print("time : ",b-a)
