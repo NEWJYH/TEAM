1d8d4ed9b9a92620e67b79e6d3ec9b8219393885
dlswns6532 <hij092919@gmail.com>
2022-12-01 14:31:26 +0900

    Add a /live page

--- projectC/main.py
+++ projectC/main.py
@@ -1,6 +1,6 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Depends
-from routers import index, CRUD, cv_live, video
+from routers import index, CRUD, cv_live, live, video
 from fastapi.staticfiles import StaticFiles
 from database import engine
 import models, database
@@ -26,6 +26,7 @@ app.mount("/static", StaticFiles(directory="static") ,name="static")
 app.include_router(index.router)
 app.include_router(CRUD.router)
 app.include_router(cv_live.router)
+app.include_router(live.router)
 app.include_router(video.router)
 
 t = schedule.BackgroundTasks()
