179c6fb305b509890bfc8c199a335706c30892a4
jyh <joung5846@naver.com>
2022-12-08 10:51:55 +0900

    clean

--- projectC/main.py
+++ projectC/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 from database import engine
-from schedule import schedule
 from routers import index, CRUD, live
 import models
 
@@ -18,15 +17,3 @@ app.mount("/static", StaticFiles(directory="static") , name="static")
 app.include_router(index.router)
 app.include_router(CRUD.router)
 app.include_router(live.router)
-
-t = schedule.BackgroundTasks()
-t.start()
-
-
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=['http://127.0.0.1:8000'],
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# ) 
